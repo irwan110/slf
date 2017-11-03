3d3531de25418948267c5e4962837a95b96f2266
irwan110 <irwantea67@gmail.com>
2017-11-04 05:33:26 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -23,7 +23,7 @@ var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1")\{xxx="/2017/10/getting-married-beauty-preparations-you.html"}
 if(x=="2")\{xxx="/2017/10/finding-beauty-within-while-dieting.html"}
-if(x=="3")\{xxx="/2017/10/dieting-for-success-making-it-happen.html"}a_to_ve[i].href="http://www.safelink.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3")\{xxx="/2017/10/dieting-for-success-making-it-happen.html"}a_to_ve[i].href="http://www.isbsafelink.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
